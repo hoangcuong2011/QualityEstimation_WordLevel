5de41dc0b882a75f0342901f67127fa77b73ff35
Hoang Cuong <hoangcuong2011@gmail.com>
2018-01-22 16:16:17 -0500

    Update WordPreprocess.py

--- WordPreprocess.py
+++ WordPreprocess.py
@@ -8,23 +8,27 @@ def main():
 	
 	measurements = []
 	
-	with open("train.features") as f:
+	with open("dev.features") as f:
+		l = set()
 		for line in f:
 			line = line.strip()
 			if(len(line)>0):
 				list = {}
 				arr = line.split("\t")
 				#for i in range(len(arr)):
-				for i in range(7): #the maximum number of features I should add
+				for i in range(4): #the maximum number of features I should add
 					#print("hoang cuong", arr[i])
 					#7.0	3.0	2.33333333333	140	<s>	Interferons	140	<s>	Interferone	0	0	0	1	2	1	0.6	0.4	0.6	5	1	CD	CARD	140|<s>	140|Interferons	140|140	CD|CARD	140|<s>|140	140|Interferons|140	
 					if i==0 or i==1 or i==2 or i==9 or i==10 or i==11 or i==12 or i==13 or i==14 or i==15 or i==16 or i==17 or i==18 or i==19:
 						list['feature'+str(i)] = float(arr[i])
 					else:
 						list['feature'+str(i)] = (arr[i])
+					if(i==3):
+						if arr[i] not in l:
+							l.add((arr[i]))
 				#print(list)
 				measurements.append(list)			
-
+	print(len(l))
 	vec = DictVectorizer()
 	array = vec.fit_transform(measurements).toarray()
 	print(array)
